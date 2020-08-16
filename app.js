a8ee6fa4c1d25e6bc2ae30eec5f02e311450e5d3
ejjoj <szymaskibartosz@gmail.com>
2020-08-16 14:18:35 +0200

    Checkbox built.

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-// WRACAMY DO KODU
-console.log('test');
+const PositiveMessage = () => <p>Możesz obejrzeć film.</p>;
+const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu, jeśli masz mniej niż 16 lat.</p>;
+
+class CheckboxAgeConfirmation extends React.Component {
+
+  state = {
+    isConfirmed: false,
+  }
+
+  handleCheckboxChange = () => {
+    this.setState({
+      isConfirmed: !this.state.isConfirmed
+    });
+  }
+
+  displayMessage = () => {
+    if (this.state.isConfirmed) {
+      return <PositiveMessage/>;
+    } else {
+      return <NegativeMessage/>;
+    }
+  }
+
+  render() {
+    return(
+      <>
+        <h1>Kup bilet na horror roku!</h1>
+        <input id='age' type="checkbox" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
+        <label htmlFor='age'>Mam conajmniej 16 lat</label>
+        {this.displayMessage()}
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<CheckboxAgeConfirmation/>, document.getElementById('root'));
