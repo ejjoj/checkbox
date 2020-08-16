552a4a891b3afedde3a27ac44774f13346b8196f
ejjoj <szymaskibartosz@gmail.com>
2020-08-16 15:18:43 +0200

    Added: form-validation

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
 const PositiveMessage = () => <p>Możesz obejrzeć film.</p>;
 const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu, jeśli masz mniej niż 16 lat.</p>;
 
-class CheckboxAgeConfirmation extends React.Component {
+class TicketShop extends React.Component {
 
   state = {
     isConfirmed: false,
+    isFormSubmitted: false
   }
 
   handleCheckboxChange = () => {
     this.setState({
-      isConfirmed: !this.state.isConfirmed
+      isConfirmed: !this.state.isConfirmed,
+      isFormSubmitted: false
     });
   }
 
   displayMessage = () => {
-    if (this.state.isConfirmed) {
-      return <PositiveMessage/>;
+    if (this.state.isFormSubmitted) {
+      if (this.state.isConfirmed) {
+        return <PositiveMessage/>;
+      } else {
+        return <NegativeMessage/>;
+      }
     } else {
-      return <NegativeMessage/>;
+      return null;
+    }
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.isFormSubmitted) {
+      this.setState({
+        isFormSubmitted: true
+      });
     }
   }
 
   render() {
     return(
       <>
-        <h1>Kup bilet na horror roku!</h1>
-        <input id='age' type="checkbox" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
-        <label htmlFor='age'>Mam conajmniej 16 lat</label>
-        {this.displayMessage()}
+        <form onSubmit={this.handleFormSubmit}>
+          <h1>Kup bilet na horror roku!</h1>
+          <input id='age' type="checkbox" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
+          <label htmlFor='age'>Mam conajmniej 16 lat</label>
+          <br />
+          <br />
+          <button>Kup bilet</button>
+          {this.displayMessage()}
+        </form>
       </>
     )
   }
 }
 
-ReactDOM.render(<CheckboxAgeConfirmation/>, document.getElementById('root'));
+ReactDOM.render(<TicketShop />, document.getElementById('root'));
